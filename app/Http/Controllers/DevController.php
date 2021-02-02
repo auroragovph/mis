9476398ca719b8fd249007185883ec1ad34adca7
jmprns <jp.pagapulan@gmail.com>
2021-02-02 16:50:27 +0800

    updates

--- app/Http/Controllers/DevController.php
+++ app/Http/Controllers/DevController.php
@@ -2,18 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use EmployeeFactory;
 use App\Models\Account;
 use Illuminate\Http\Request;
-use EmployeeFactory;
-use Modules\FileManagement\Entities\Document\FMS_Document;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Schema;
 use Modules\FileTracking\Entities\FTS_QR;
 use Modules\HumanResource\Entities\HR_Employee;
-use Spatie\Permission\Models\Role;
+use Modules\FileManagement\Entities\Document\FMS_Document;
 
 class DevController extends Controller
 {
     public function index()
     {
-        
+        DB::statement("SET FOREIGN_KEY_CHECKS = 0");
+            $tables = DB::select('SHOW TABLES');
+
+            // dd($tables);
+        foreach($tables as $table){
+            Schema::drop($table->Tables_in_mis);
+            echo 'Table '.$table->Tables_in_mis.' Droped. <br>';
+        }
+        DB::statement("SET FOREIGN_KEY_CHECKS = 1");
     }
 }

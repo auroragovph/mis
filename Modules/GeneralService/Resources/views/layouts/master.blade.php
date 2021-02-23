4df2760afc9e10952d2037e49aa59fb432b98d8f
jmprns <jp.pagapulan@gmail.com>
2021-02-23 08:26:55 +0800

    create general service module

---
+++ Modules/GeneralService/Resources/views/layouts/master.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Module GeneralService</title>
+
+       {{-- Laravel Mix - CSS File --}}
+       {{-- <link rel="stylesheet" href="{{ mix('css/generalservice.css') }}"> --}}
+
+    </head>
+    <body>
+        @yield('content')
+
+        {{-- Laravel Mix - JS File --}}
+        {{-- <script src="{{ mix('js/generalservice.js') }}"></script> --}}
+    </body>
+</html>

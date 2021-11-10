f3d519c4abf5c2342ae76e81db4c7e9c00910069
Jimwell Parinas <jp.pagapulan@gmail.com>
2021-11-10 11:32:31 +0800

    updates

--- app/Http/Controllers/DevController.php
+++ app/Http/Controllers/DevController.php
@@ -8,6 +8,17 @@ class DevController extends Controller
 {
     public function __invoke(Request $request)
     {
-    
+        $fields = [
+            'personal.firstName' => 'Jimwell'
+        ];
+
+        $pdf = new \FPDM(base_path('pdf/form.pdf'));
+
+        // dd($pdf);
+
+        $pdf->useCheckboxParser = true; // Checkbox parsing is ignored (default FPDM behaviour) unless enabled with this setting
+        $pdf->Load($fields, true);
+        $pdf->Merge();
+        $pdf->Output();
     }
 }
